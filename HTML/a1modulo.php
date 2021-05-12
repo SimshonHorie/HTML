4e746f77a3db44d57fc874040b9861e725c7fdf2
SimshonHorie <simshon.horie@gmail.com>
2021-05-12 19:02:24 -0300

    html
    Projeto html

---
+++ HTML/a1modulo.php
@@ -0,0 +1,188 @@
+﻿<!DOCTYPE html!>
+<html>
+	<head> 
+		<title> Etec </title> 		
+		<link rel="stylesheet" href="css.css" type="text/css"/>
+        <!-- Permite que a pagina seja responsiva -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- CSS do Bootstrap -->
+        <link rel="stylesheet" href="css/bootstrap.css">
+
+       
+
+        <!-- JQuery framework JS (Adiciona recursos ao JavaScript) -->
+        <script src="js/jquery-1.12.4.min.js"></script>
+
+        <!-- JS do Bootstrap -->
+        <script src="js/bootstrap.js"></script>
+         <style>
+		 #fundo {
+	         background-color: #36648b;
+	
+	
+           }
+		    #conteudo {
+		 position: relative;
+         margin:0px auto 0px auto;
+         width:100%;
+         height:1100px;
+         float:left;
+         background-color:white;
+         margin-left:0px;
+		 margin-top: 0px;
+		 font-size: 18;
+		 
+}
+		 </style>		
+		
+	</head>
+	<body id="fundo">
+	<div class="container">
+	 
+	          <div class="row">
+	             <div class=" col-sm-12 col-xs-12">
+	                   <a href="home.php" class="thumbnail"> <img src="etec1.png" class="img-responsive" alt="..."></a>
+	              </div>
+	          </div>
+		  
+	<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+   
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <ul class="nav navbar-nav">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" style="color:black !important">Informática para Internet <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a style="color:black !important" href="i1modulo.php">1ºModulo</a></li>
+			<li role="separator" class="divider"></li>
+            <li><a style="color:black !important" href="i2modulo.php">2ºModulo</a></li>
+			<li role="separator" class="divider"></li>
+            <li><a style="color:black !important" href="i3modulo.php">3ºModulo</a></li>
+			
+            
+          </ul>
+        </li>
+		<li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" style="color:black !important">ETIM - Info 
+		  <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a style="color:black !important" href="e1modulo.php">1ºETIM</a></li>
+			<li role="separator" class="divider"></li>
+            <li><a style="color:black !important" href="e2modulo.php">2ºETIM</a></li>
+			<li role="separator" class="divider"></li>
+            <li><a style="color:black !important" href="e3modulo.php">3ºETIM</a></li>
+			
+            
+          </ul>
+        </li>
+		<li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" style="color:black !important">ETIM - Agro 
+		  <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a style="color:black !important" href="ea1modulo.php">1ºETIM</a></li>
+			<li role="separator" class="divider"></li>
+            <li><a style="color:black !important" href="ea2modulo.php">2ºETIM</a></li>
+			<li role="separator" class="divider"></li>
+            <li><a style="color:black !important" href="ea3modulo.php">3ºETIM</a></li>
+			
+            
+          </ul>
+        </li>
+		<li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" style="color:black !important">Química <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a style="color:black !important" href="q1modulo.php">1ºMódulo</a></li>
+			<li role="separator" class="divider"></li>
+            <li><a style="color:black !important" href="q2modulo.php">2ºMódulo</a></li>
+			<li role="separator" class="divider"></li>
+            <li><a style="color:black !important" href="q3modulo.php">3ºMódulo</a></li>
+			<li role="separator" class="divider"></li>
+			<li><a style="color:black !important" href="q4modulo.php">4ºMódulo</a></li>
+			
+            
+          </ul>
+        </li>
+		<li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" style="color:black !important">Agroindústria<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a style="color:black !important" href="a1modulo.php">1ºMódulo</a></li>
+			<li role="separator" class="divider"></li>
+            <li><a style="color:black !important" href="a2modulo.php">2ºMódulo</a></li>
+			<li role="separator" class="divider"></li>
+            <li><a style="color:black !important" href="a3modulo.php">3ºMódulo</a></li>
+			
+            
+          </ul>
+        </li>
+		<li >
+          <a href="home.php"    style="color:black !important">Home </a></li>
+		  <li >
+          <a href="login.php"    style="color:black !important">Sair </a></li>
+      </ul>
+      
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<div>
+         
+           <article id="conteudo">
+		 		   <div class="row">
+	              <div class="col-sm-offset-3 col-sm-9 col-xs-12">
+				<table > 
+				<tr>
+					<td colspan=2 height="30">
+					</td>
+				</tr>
+
+				<tr>
+					<td width="140" height="30"> <a href="a_ltt.php" class="thumbnail"> <img  src="pasta.png"/></a>
+					<td width="300" height="30"><a href="a_ltt.php">Linguagem, Trabalho e Tecnologia</a></td>
+				</tr>
+					
+				</tr>
+				<tr>
+					<td width="140" height="30"><a href="a_qa.php" class="thumbnail"> <img  src="pasta.png"/></a>
+					<td width="300" height="30"><a href="a_qa.php">Química Analítica</a>  </td>
+				</tr>
+					
+				</tr>
+				<tr>
+					<td width="140" height="30"> <a href="a_mpa.php" class="thumbnail"> <img  src="pasta.png"/></a>
+					<td width="300" height="30"><a href="a_mpa.php">Microbiologia na Produção Agroindustrial<a/></td>
+				</tr>
+					
+				</tr>
+				<tr>
+					<td width="140" height="30"> <a href="a_opmpov.php" class="thumbnail"> <img  src="pasta.png"/></a>
+					<td width="300" height="30"><a href="a_opmpov.php"> Obtenção e Preparo da Matéria Prima de Origem Vegetal</a> </td>
+				</tr>
+					
+				</tr>
+				<tr>
+					<td width="140" height="30"> <a href="a_opmpoa.php" class="thumbnail"> <img  src="pasta.png"/></a>
+					<td width="300" height="30"><a href="a_opmpoa.php">Obtenção e Preparo da Matéria Prima de Origem Animal </a> </td>
+				</tr>
+					
+				</tr>
+				<tr>
+					<td width="140" height="30"> <a href="a_ai.php" class="thumbnail"> <img  src="pasta.png"/></a>
+					<td width="300" height="30"><a href="a_ai.php">Aplicativos Informatizados</a></td>
+				</tr>
+				
+					
+				</table>
+          </div>
+		  </div>
+</article>
+
+<footer id="rodape">
+				<p>Auxilio ao Professor</p>
+			</footer>   
+		
+				
+	</body>
+</html>
